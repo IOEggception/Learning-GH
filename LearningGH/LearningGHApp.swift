ad925513ab1abd23bbbcee18c9297fc9f296f61b
Sean Lacey <sean.lacey@libertymutual.com>
2023-03-08 10:45:41 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Sean.Lacey on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
